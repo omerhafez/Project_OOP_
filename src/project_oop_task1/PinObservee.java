24b1dd3b441728971f74ee039bfe4d922745dec9
omar hafez fox <omerhafez778@gmail.com>
2020-04-19 21:09:34 +0200

    This Is Task_1

---
+++ src/project_oop_task1/PinObservee.java
@@ -0,0 +1,18 @@
+package project_oop_task1;
+public class PinObservee {
+    
+    PinObserver obj;
+    public void RegisterObserver()
+    {
+    
+    }
+    public void DeleteObserver()
+    {
+    
+    }
+    public void Notify()
+    {
+    
+    }
+    
+}
